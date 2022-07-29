9434e0704f72dcff4f1a6e6a449e449d2245e2a0
David <kim.dv@phystech.edu>
2022-07-30 01:09:03 +1000

    Сопоставляются модельный и экспериментальный спектры вторых производных. Пока без фиттинга

--- Modulation.py
+++ Modulation.py
@@ -8,10 +8,6 @@ from scipy.optimize import curve_fit
 import math
 
 
-def lin_comb_iso4(x, y1, y2, y3, y4, y5, y6, y7, y8):
-    return x[0] * y1 + x[1] * y2 + x[2] * y3 + x[3] * y4 + x[4] * y5 + x[5] * y6 + x[6] * y7 + x[7] * y8
-
-
 def lin_comb_iso1(x, y1, y2):
     return x[0] * y1 + x[1] * y2
 
@@ -20,25 +16,13 @@ def exponential_comb_iso1(x, y1, y2):
     return np.exp((- x[0] * y1 - x[1] * y2) * l)
 
 
-def baseline(a, b, c):
-    return a * nu_period1 + b + c * (nu_period1 ** 2)
-
-
-def normalized(a, b, c):
-    return exper_period1 / (a * nu_period1 + b + c * (nu_period1 ** 2))
-
-
 def nu_adjust(nu_0):
-    return np.arange(nu_0, nu_0 - 0.0005 + len(exper_period1) * 0.001, 0.001)
+    return np.arange(nu_0, nu_0 - 0.0005 + len(deriv_period) * 0.001, 0.001)
 
 
-def search(x, a, b, c):
-    return a * x * x + b * x + c
-
-
-nu_step = 0.048
+nu_step = 0.0485
 beginning = 0  # Номер первого пика
-beginning1 = 48
+beginning1 = 48  # Номер первого пика для спектра первых гармоник
 beginning2 = 0
 beginning3 = 0
 beginning4 = 0
@@ -50,6 +34,7 @@ fabri = [float(i) for i in open("Modulation/fpmod").read().split()]
 exper = [float(i) for i in open("Modulation/1mod").read().split()]
 hitran = pd.read_excel("3562-3564/3562.xlsx")
 Trans = np.array(hitran.Trans)
+Trans = np.gradient(np.gradient(Trans, 0.01), 0.01)
 nu_model = np.array(hitran.nu)
 
 max_fabri = max(fabri)
@@ -77,6 +62,7 @@ fabri1 = []
 fabri2 = []
 fabri3 = []
 fabri4 = []
+deriv = []
 i = 0
 while i < len(exper_cut1) - 3:
     exper1.append(exper_cut1[i])
@@ -91,13 +77,17 @@ while i < len(exper_cut1) - 3:
     exper4.append(exper_cut1[i + 3])
     fabri4.append(fabri_cut[i + 3])
     points4.append(i + 3)
+    deriv.append(2 * (exper_cut1[i + 3] + exper_cut1[i + 1] - 2 * exper_cut1[i]) / (exper_cut1[i] + exper_cut1[i + 2]))
     i += 4
+
 
 interp_grid1 = np.arange(0, end - 4)
 interp_signal1 = interp1d(points1, exper1, kind='quadratic')
 signal_interp1 = interp_signal1(interp_grid1)
 interp_fabri1 = interp1d(points1, fabri1, kind='quadratic')
 fabri_interp1 = interp_fabri1(interp_grid1)
+interp_deriv = interp1d(points1, deriv, kind='quadratic')
+deriv_interp = interp_deriv(interp_grid1)
 interp_grid2 = np.arange(1, end - 3)
 interp_signal2 = interp1d(points2, exper2, kind='quadratic')
 signal_interp2 = interp_signal2(interp_grid2)
@@ -114,7 +104,7 @@ signal_interp4 = interp_signal4(interp_grid4)
 interp_fabri4 = interp1d(points4, fabri4, kind='quadratic')
 fabri_interp4 = interp_fabri4(interp_grid4)
 
-plt.plot(interp_grid1, fabri_interp1)
+plt.plot(interp_grid1, deriv_interp)
 plt.show()
 #plt.plot(interp_grid2, fabri_interp2)
 #plt.show()
@@ -130,16 +120,15 @@ for i in range(beginning1, end - 6):
         pointmax.append(i)
         valuemax.append(valuemax[len(valuemax) - 1] + nu_step / 2)
         a = i
-#pointmax.append(end - 4)
-#valuemax.append(valuemax[len(valuemax) - 1] + nu_step / 2)
 
 exper_cut1 = signal_interp1[beginning1:a]
 interp_grid_nu1 = np.arange(beginning1, pointmax[len(pointmax) - 1])
 interp_nu1 = interp1d(pointmax, valuemax, kind='quadratic')
 nu_interp1 = interp_nu1(interp_grid_nu1)
+deriv_cut = deriv_interp[beginning1:a]
 
-#plt.plot(np.arange(len(nu_interp1)), nu_interp1)
-#plt.show()
+plt.plot(np.arange(len(nu_interp1)), nu_interp1)
+plt.show()
 
 # Разворот и интерполяция спектра на периодическую сетку частот
 nu_reversed1 = [nu_interp1[len(nu_interp1) - i - 1] for i in range(len(nu_interp1))]
@@ -147,43 +136,28 @@ exper_reversed1 = [exper_cut1[len(exper_cut1) - i - 1] for i in range(len(exper_
 nu_period1 = np.arange(nu_reversed1[0], 0, -0.001)
 interp_exper1 = interp1d(nu_reversed1, exper_reversed1, kind='quadratic')
 exper_period1 = interp_exper1(nu_period1)
+deriv_reversed = [deriv_cut[len(deriv_cut) - i - 1] for i in range(len(deriv_cut))]
+interp_deriv = interp1d(nu_reversed1, deriv_reversed, kind='quadratic')
+deriv_period = interp_deriv(nu_period1)
 
 
-# Подбор базовой линии
+# Подбор частоты
 def sliders_on_changed(val):
-    line.set_ydata(baseline(a_slider.val, b_slider.val, c_slider.val))
     exp.set_xdata(nu_adjust(nu_slider.val))
-    exp.set_ydata(normalized(a_slider.val, b_slider.val, c_slider.val))
+    exp.set_ydata(deriv_period)
     fig.canvas.draw_idle()
 
 
 fig = plt.figure()
-ax = fig.add_subplot(121)
-bx = fig.add_subplot(122)
-fig.subplots_adjust(bottom=0.25)
-a_0 = 5028.72
-b_0 = 1301.86
-c_0 = -984.24
+ax = fig.add_subplot(111)
+fig.subplots_adjust(bottom=0.15)
 nu_0 = 3561.36
-[line, approximation] = ax.plot(nu_period1, baseline(a_0, b_0, c_0), nu_period1, exper_period1, linewidth=2)
-[exp, model] = bx.plot(nu_adjust(nu_0), normalized(a_0, b_0, c_0), nu_model, Trans)
-bx.set_ylim(0, 1)
-a_slider_ax = fig.add_axes([0.25, 0.15, 0.65, 0.03])
-a_slider = Slider(a_slider_ax, 'a', 0, 10000, valinit=a_0)
-b_slider_ax = fig.add_axes([0.25, 0.1, 0.65, 0.03])
-b_slider = Slider(b_slider_ax, 'b', 0, 10000, valinit=b_0)
-c_slider_ax = fig.add_axes([0.25, 0.05, 0.65, 0.03])
-c_slider = Slider(c_slider_ax, 'c', -5000, 5000, valinit=c_0)
-nu_slider_ax = fig.add_axes([0.25, 0, 0.65, 0.03])
+[exp, model] = ax.plot(nu_adjust(nu_0), deriv_period, nu_model, Trans)
+#ax.set_ylim(-1, 1)
+nu_slider_ax = fig.add_axes([0.15, 0.05, 0.65, 0.03])
 nu_slider = Slider(nu_slider_ax, 'nu', nu_0 - 1, nu_0 + 1, valinit=nu_0)
 nu_slider.on_changed(sliders_on_changed)
-a_slider.on_changed(sliders_on_changed)
-b_slider.on_changed(sliders_on_changed)
-c_slider.on_changed(sliders_on_changed)
 plt.show()
-print(a_slider.val, b_slider.val, c_slider.val)
-for i in range(len(exper_period1)):
-    exper_period1[i] = exper_period1[i] / (a_slider.val * nu_period1[i] + b_slider.val + c_slider.val * (nu_period1[i] ** 2))
 print(nu_slider.val)
 nu = nu_adjust(nu_slider.val)
 nu_end = nu[len(nu) - 1]
@@ -240,41 +214,6 @@ for i in range(len(nuij)):
     khapi[molec[i] - 1, iso[i] - 1, :] += S * hapi.PROFILE_VOIGT(nuij[i], AlphaD, Gamma, p * Delta[i], nu)
     #khapi[molec[i] - 1, iso[i] - 1, :] += S * hapi.PROFILE_VOIGT(nuij[i], AlphaD, Gamma, 0, nu)
 
-"""
-x = [khapi[0, 0, :], khapi[0, 1, :], khapi[0, 2, :], khapi[0, 3, :], khapi[1, 0, :], khapi[1, 1, :], khapi[1, 2, :], khapi[1, 3, :]]
-exper_linear = - np.log(exper_period1) / l
-los, cov = curve_fit(lin_comb_iso4, x, exper_linear, p0=1e13 * np.ones(8), bounds=[1e8, 1e22])
-
-x = [khapi[0, 0, :], khapi[1, 0, :]]
-#exper_linear = - np.log(exper_period1) / l
-#los, cov = curve_fit(lin_comb_iso1, x, exper_linear, p0=1e13 * np.ones(2), bounds=[1e8, 1e22])
-los, cov = curve_fit(exponential_comb_iso1, x, exper_period1, p0=1e13 * np.ones(2), bounds=[1e8, 1e22])
-print(los)
-
-los[0] = 1.909e17
-los[1] = 1.02e16
-for i in range(iso_num):
-    thapi += los[i] * khapi[0, i, :] + los[i + iso_num] * khapi[1, i, :]
-thapi = np.exp(- l * thapi)
-plt.plot(nu, thapi, nu_model, Trans)
-plt.show()
-
-plt.plot(nu, (thapi - exper_period1) / exper_period1)
-plt.show()
-
-plt.plot(np.arange(len(exper_period1)), exper_period1 / thapi)
-plt.show()
-
-koefs, covar = curve_fit(search, nu_period1, exper_period1 / thapi, p0=5000 * np.ones(3), bounds=[-1e4, 1e4])
-print(koefs)
-
-new_baseline = search(nu_period1, koefs[0], koefs[1], koefs[2])
-plt.plot(np.arange(len(exper_period1)), exper_period1 / thapi, np.arange(len(exper_period1)), new_baseline)
-plt.show()
-
-for i in range(len(exper_period1)):
-    exper_period1[i] = exper_period1[i] / (koefs[1] * nu_period1[i] + koefs[2] + koefs[0] * (nu_period1[i] ** 2))
-"""
 
 thapi = np.zeros(len(nu))
 cut_len = int(len(nu) - 180)
